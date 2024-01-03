9e0c583aaa1d7b28a64b3247a47e9749d57b5f00
Andy Dobbs <andrew.dobbs@gmail.com>
2024-01-03 15:50:53 -0500

    Initial Commit

---
+++ ios-course-ex-1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios-course-ex-1
+//
+//  Created by Andy Dobbs on 1/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios-course-ex-1/ios_course_ex_1App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_course_ex_1App.swift
+//  ios-course-ex-1
+//
+//  Created by Andy Dobbs on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_course_ex_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
