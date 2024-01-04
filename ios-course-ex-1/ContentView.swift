f4fc824a4bf4ebcff3797699284e7ce18f7eeaa3
Andy Dobbs <andrew.dobbs@gmail.com>
2024-01-03 19:48:22 -0500

    Finish exercise

--- ios-course-ex-1/ContentView.swift
+++ ios-course-ex-1/ContentView.swift
@@ -5,19 +5,16 @@
 //  Created by Andy Dobbs on 1/3/24.
 //
 
-// Make an HStack with two circles and a rectangle
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Tap me")
-            .font(.largeTitle)
-            .padding()
-            .background(Color.green)
-            .foregroundStyle(.white)
-            .clipShape(RoundedRectangle(cornerRadius: 20))
-            .kerning(/*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Rectangle()
+            Circle()
+                .foregroundColor(.blue)
+            Text("Hello")
+        }
     }
 }
 
